18c629acea48e0905356c47caa47aad44854ca78
JohnnyMastricht <johnnymastricht@gmail.com>
2018-11-09 15:43:07 +0200

    more improvements

--- socks_proxy.sh
+++ socks_proxy.sh
@@ -3,11 +3,11 @@
 source ../proxy_servers
 	case $1 in
 	    LA | La | la | lA)
-		  PID=$(lsof -i:1080 | grep ssh | awk {'print $2'} | uniq )
+		  PID=$(lsof -i:$proxyport | grep ssh | awk {'print $2'} | uniq )
 		  if [ -d /proc/$PID/cwd ]; then echo -e "It seems that the connection is already established. Exiting..."
 		  	else
 			    read -p "Enter login:" login
-			    echo "calling to Mephistopheles of Los Angeles..."
+			    echo "calling Mephistopheles of Los Angeles..."
         		ssh -f -C2qTnN -D $proxyport $login@$laserver -p$sshport
         		if [ $? -eq 0 ]; then echo "The connection has been established"
                 else 
@@ -16,7 +16,7 @@ source ../proxy_servers
           fi
 	;;
 	    LY | Ly | ly | lY)
-          PID=$(lsof -i:1080 | grep ssh | awk {'print $2'} | uniq )
+          PID=$(lsof -i:$proxyport | grep ssh | awk {'print $2'} | uniq )
 		  if [ -d /proc/$PID/cwd ]; then echo -e "It seems that the connection is already established. Exiting..."
 		     else
                read -p "Enter login:" login
@@ -29,13 +29,13 @@ source ../proxy_servers
           fi
 	;;
 		close)
-			PID=$(lsof -i:1080 | grep ssh | awk {'print $2'} | uniq )
+			PID=$(lsof -i:$proxyport | grep ssh | awk {'print $2'} | uniq )
 			if [ -d /proc/$PID/cwd ]; then kill -9 $PID && echo -e "Connection has been terminated successfully"
 			else echo "Something went wrong! Please check manually."
 	        fi
 	;;
 		status)
-			PID=$(lsof -i:1080 | grep ssh | awk {'print $2'} | uniq )
+			PID=$(lsof -i:$proxyport | grep ssh | awk {'print $2'} | uniq )
 			if [ -d /proc/$PID/cwd ]; then echo -e "Socks proxy should be running on port 1080 with PID '$PID'"
 			else echo "Seems that Socks proxy is not running"
 	        fi
